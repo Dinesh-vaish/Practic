83f013a8f19a1f3432a4e68f210a5126269d3f68
Dinesh-vaish <dineshbais2005@gmail.com>
2026-06-14 13:36:55 +0530

    this a correct code

--- javascript/sumOfDigit.js
+++ javascript/sumOfDigit.js
@@ -3,11 +3,11 @@ Qustions @: Sum of digits of a number
 1287: 1+2+8+7=18
 */
  function sumOfDigits(num){
-    let sum=0;
+    let sum = 0;
     while(num>0)
     {
-        sum +=nums%10;
-        sum =num/10;
+        sum +=num%10;
+        num =Math.floor(num/10);
     }
     return sum;
  }
