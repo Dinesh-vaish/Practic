c00356333059091e1625519b5ec0e5d15838aa28
Dinesh vaish <dineshbais2005@gmail.com>
2026-06-30 11:42:06 +0530

    Time: 0 ms (100%), Space: 32.8 MB (99.64%) - LeetHub

---
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        int gcd=0;
+       for(int i:nums)
+       {
+        gcd=__gcd(gcd,i);
+        if(gcd==1) return 1;
+       }
+       return 0;
+    }
+};
